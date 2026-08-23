19672d589c2ddc23ec5828506cd5caaa9ed76b67
sabermahmud <sabermahmud01@gmail.com>
2026-08-23 06:56:08 +0300

    problem 3 and problem 4 is solved

--- a4/script/a4.js
+++ a4/script/a4.js
@@ -1,13 +1,6 @@
 "use strict";
 /*
 Problem 1 — Battery Level Status
-তোমাকে এমন একটি function বানাতে হবে যেখানে battery percentage দিলে সেটার ওপর ভিত্তি করে একটি status text ফেরত দেবে।
-যেমন:
-10% হলে → "Low"
-35% হলে → "Medium"
-75% হলে → "High"
-100% হলে → "Full"
-অর্থাৎ percentage-এর কোন range-এ পড়ছে, তার ওপর নির্ভর করে একটি নির্দিষ্ট status return করতে হবে।
 */
 const getBatteryStatus = (batteryLife) => {
     if (batteryLife <= 20) {
@@ -24,29 +17,25 @@ const getBatteryStatus = (batteryLife) => {
 const formatBookingConfirmation = (booking) => {
     return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`;
 };
-console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }));
 /*
 Problem 3 — Weekly Expense Tracker
-এখানে সপ্তাহের বিভিন্ন খরচ একটি array হিসেবে দেওয়া হবে।
-তোমার function-এর কাজ হলো সবগুলো expense যোগ করে মোট খরচ return করা।
-যেমন:
-[200, 450, 100]
-এর মোট হবে:
-750
-আর যদি কোনো expense না থাকে:
-[]
-তাহলে মোট হবে:
-0
 */
-/*
-Problem 4 — Traffic Light Action
-এখানে traffic light-এর তিনটি possible color আছে:
-"red" → "Stop"
-"yellow" → "Slow Down"
-"green" → "Go"
-তোমার function একটি traffic light color পাবে এবং সেই color অনুযায়ী নির্দিষ্ট action return করবে।
-আর assignment-এর বিশেষ requirement হলো, Light নামে একটি union type আগে তৈরি করতে হবে।
-*/
+const calculateWeeklyTotal = (expenses) => {
+    const totalExpenses = expenses.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+    return totalExpenses;
+};
+const getTrafficAction = (light) => {
+    if (light === "red") {
+        return "Stop";
+    }
+    else if (light === "yellow") {
+        return "Slow Down";
+    }
+    return "Go";
+};
+console.log(getTrafficAction("red"));
 /*
 Problem 5 — Quiz Score Summary
 এখানে একজন student's quiz-এর সব score একটি array হিসেবে দেওয়া হবে।
--- a4/script/a4.ts
+++ a4/script/a4.ts
@@ -1,12 +1,5 @@
 /*
 Problem 1 — Battery Level Status
-তোমাকে এমন একটি function বানাতে হবে যেখানে battery percentage দিলে সেটার ওপর ভিত্তি করে একটি status text ফেরত দেবে।
-যেমন:
-10% হলে → "Low"
-35% হলে → "Medium"
-75% হলে → "High"
-100% হলে → "Full"
-অর্থাৎ percentage-এর কোন range-এ পড়ছে, তার ওপর নির্ভর করে একটি নির্দিষ্ট status return করতে হবে।
 */
 const getBatteryStatus = (batteryLife: number): string => {
   if (batteryLife <= 20) {
@@ -20,41 +13,26 @@ const getBatteryStatus = (batteryLife: number): string => {
 };
 /*
 Problem 2 — Table Booking Confirmation
-এখানে একটি restaurant booking-এর তথ্য থাকবে:
-customer's name
-কতজন guests
-booking-এর time
-এই তিনটি তথ্য ব্যবহার করে একটি সম্পূর্ণ confirmation sentence return করতে হবে।
-যেমন booking-এর তথ্য যদি হয়:
-Aisha, 4 guests, 7:00 PM
-তাহলে return হবে:
-Aisha's table for 4 guests is confirmed at 7:00 PM.
-গুরুত্বপূর্ণ বিষয় হলো, sentence-এর মধ্যে দেওয়া তথ্যগুলো booking object থেকেই আসতে হবে। নিজের মতো করে "Aisha" বা "4" লিখে দেওয়া যাবে না।
 */
 interface Booking {
-    name : string;
-    guests: number;
-    time: string;
+  name: string;
+  guests: number;
+  time: string;
 }
-const formatBookingConfirmation = (booking : Booking):string => {
-return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`
-}
-console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }
+const formatBookingConfirmation = (booking: Booking): string => {
+  return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`;
+};
 
-))
 /*
 Problem 3 — Weekly Expense Tracker
-এখানে সপ্তাহের বিভিন্ন খরচ একটি array হিসেবে দেওয়া হবে।
-তোমার function-এর কাজ হলো সবগুলো expense যোগ করে মোট খরচ return করা।
-যেমন:
-[200, 450, 100]
-এর মোট হবে:
-750
-আর যদি কোনো expense না থাকে:
-[]
-তাহলে মোট হবে:
-0
 */
+const calculateWeeklyTotal = (expenses: number[]): number => {
+  const totalExpenses = expenses.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+
+  return totalExpenses;
+};
 
 /*
 Problem 4 — Traffic Light Action
@@ -65,7 +43,17 @@ Problem 4 — Traffic Light Action
 তোমার function একটি traffic light color পাবে এবং সেই color অনুযায়ী নির্দিষ্ট action return করবে।
 আর assignment-এর বিশেষ requirement হলো, Light নামে একটি union type আগে তৈরি করতে হবে।
 */
-
+type Light = "red" | "yellow" | "green"
+const getTrafficAction = (light: Light): string => {
+    if(light === "red"){
+        return "Stop"
+    }
+    else if (light === "yellow"){
+        return "Slow Down"
+    }
+  return "Go";
+};
+console.log(getTrafficAction("red"));
 /*
 Problem 5 — Quiz Score Summary
 এখানে একজন student's quiz-এর সব score একটি array হিসেবে দেওয়া হবে।
